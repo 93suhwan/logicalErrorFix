c3c5655982a23478f721757877db5af17126f487
Suhwan Ji <93suhwan@gmail.com>
2023-01-29 18:28:17 +0900

    Update files

--- convert_riegeli2cpp.py
+++ convert_riegeli2cpp.py
@@ -1,10 +1,13 @@
 import io
+import os
 import sys
 
 import riegeli
 
-DIR='./data/'
-FILETYPE=['train', 'valid', 'test']
+import contest_problem_pb2
+
+DIR='/'.join(os.path.realpath(__file__).split('/')[0:-2]) + '/data/'
+FILETYPE=['_train', '_valid', '_test']
 
 def _all_problems(filenames):
   """Iterates through all ContestProblems in filenames."""
@@ -13,24 +16,28 @@ def _all_problems(filenames):
     for problem in reader.read_messages(contest_problem_pb2.ContestProblem):
       yield problem
 
-def make_cpp_files(filenames):
-  for fileType in FILETYPE:
-    if not os.path.exists(DIR + 'cppfiles_' + fileType):
-      os.makedirs(DIR + 'cppfiles_' + fileType)
+def make_cpp_files(filenames, fileType):
+  if not os.path.exists(DIR + 'cppfiles' + fileType):
+    os.makedirs(DIR + 'cppfiles' + fileType)
 
-    for program_id, problem in enumerate(_all_problems(filenames)):
-      for correct_id, correct in enumerate(problem.solutions):
-        if correct.language==2:
-          corrFile = open(DIR + 'cppfiles_' + fileType + '/' + str(program_id) + 'C' + str(correct_id)+'.cpp', 'w')
-          corrFile.write(correct.solution)
+  for program_id, problem in enumerate((_all_problems(filenames))):
+    for correct_id, correct in enumerate(problem.solutions):
+      if correct.language==2:
+        corrFile = open(DIR + 'cppfiles' + fileType + '/' + str(program_id) + 'C' + str(correct_id)+'.cpp', 'w')
+        corrFile.write(correct.solution)
+        corrFile.close()
                  
-      for incorrect_id, incorrect in enumerate(problem.incorrect_solutions):
-        if incorrect.language==2:                    
-          incorrFile = open(DIR + 'cppfiles_' + fileType + '/' + str(program_id) + 'I' + str(incorrect_id)+'.cpp', 'w')
-          incorrFile.write(incorrect.solution)
+    for incorrect_id, incorrect in enumerate((problem.incorrect_solutions)):
+      if incorrect.language==2:                    
+        incorrFile = open(DIR + 'cppfiles' + fileType + '/' + str(program_id) + 'I' + str(incorrect_id)+'.cpp', 'w')
+        incorrFile.write(incorrect.solution)
+        incorrFile.close()
 
 def main():
-  make_cpp_files(sys.argv[1:])
+  for fileType in FILETYPE:
+    print('Creating cppfiles' + fileType + '...')
+    filenames = [name for name in sys.argv[1:] if fileType in name]
+    make_cpp_files(filenames, fileType)
 
 if __name__ == '__main__':
   main()
--- make_code_edit_dist.py
+++ make_code_edit_dist.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
 from io import open
 
 COLUMNS = ['PID', 'Correct', 'Incorrect', 'Editdist']
@@ -15,7 +14,9 @@ def load_code(dataFrame, data_loc=''):
   # dataFrame['Input'] = ''
   # dataFrame['Output'] = ''
 
-  for idx, cont in tqdm(dataFrame.iterrows()):
+  for idx, cont in dataFrame.iterrows():
+    if idx % 5000 == 0:
+      print('Current index = ', idx)
     if dataFrame.at[idx, 'Editdist'] > THRESHOLD or dataFrame.at[idx, 'Editdist'] == 0:
       dataFrame = dataFrame.drop(idx)
     else:
@@ -26,19 +27,22 @@ def load_code(dataFrame, data_loc=''):
         cCodeWithLine = []
         iCodeWithLine = []
         stmt = None
-        for idx in range(max(len(cLines), len(iLines))):
-          if idx < len(cLines):
-            cCodeWithLine.append(str(idx + 1) + ' ' + line)
-          if idx < len(iLines):
-            iCodeWithLine.append(str(idx + 1) + ' ' + line)
+        for lIdx in range(max(len(cLines), len(iLines))):
+          if lIdx < len(cLines):
+            cCodeWithLine.append(str(lIdx + 1) + ' ' + cLines[lIdx])
+          if lIdx < len(iLines):
+            iCodeWithLine.append(str(lIdx + 1) + ' ' + iLines[lIdx])
+          print()
+          print('./data/cppfiles_' + data_loc + '/' + str(cont[COLUMNS[0]]) + COLUMNS[1][0] + str(cont[COLUMNS[1]]) + '.cpp')
+          print('./data/cppfiles_' + data_loc + '/' + str(cont[COLUMNS[0]]) + COLUMNS[2][0] + str(cont[COLUMNS[2]]) + '.cpp')
           if stmt is not None:
             continue
-          else if cCodeWithLine[idx] != iCodeWithLine[idx]:
-            if idx + 1 < len(iLines): # if a delete statement exists.
-              if cCodeWithLine[idx] == iCodeWithLine[idx + 1]:
+          elif cLines[lIdx] != iLines[lIdx]:
+            if lIdx + 1 < len(iLines): # if a delete statement exists.
+              if cLines[lIdx] == iLines[lIdx + 1]:
                 stmt = ' '
-            stmt = cCodeWithLine[idx]
-        corr.colse()
+            stmt = cLines[lIdx]
+        corr.close()
         incorr.close()
         dataFrame.at[idx, 'Correct_code'] = cCodeWithLine
         dataFrame.at[idx, 'Incorrect_code'] = iCodeWithLine
@@ -51,8 +55,10 @@ def load_code(dataFrame, data_loc=''):
 def main():
   DIR = './data/edit_distance/'
   for fileType in FILETYPE:
+    print('Creating ' + fileType + ' pairs.')
     pairedData = pd.read_csv(DIR + '/pair_solution_' + fileType + '.txt', sep='\t', names=COLUMNS)
-    load_code(pairedDataTrain, fileType).to_csv(DIR + 'pair_code_edit_dist_' + fileType + '.txt', sep='\t', index=False)
+    print('Data length = ', len(pairedData))
+    load_code(pairedData, fileType).to_csv(DIR + 'pair_code_edit_dist_' + fileType + '.txt', sep='\t', index=False)
 
 if __name__ == "__main__":
   main()
