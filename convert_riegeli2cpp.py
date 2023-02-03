d50ef08b6f5ce9a3d3aefda9e24797b55a4d53aa
Suhwan Ji <93suhwan@gmail.com>
2023-02-03 17:45:04 +0900

    Add descriptions, samples extraction functions

--- convert_riegeli2cpp.py
+++ convert_riegeli2cpp.py
@@ -20,18 +20,27 @@ def make_cpp_files(filenames, fileType):
   if not os.path.exists(DIR + 'cppfiles' + fileType):
     os.makedirs(DIR + 'cppfiles' + fileType)
 
-  for program_id, problem in enumerate((_all_problems(filenames))):
+  if not os.path.exists(DIR + 'descriptions' + fileType):
+    os.makedirs(DIR + 'descriptions' + fileType)
+
+  if not os.path.exists(DIR + 'samples' + fileType):
+    os.makedirs(DIR + 'samples' + fileType)
+
+  for program_id, problem in enumerate(_all_problems(filenames)):
     for correct_id, correct in enumerate(problem.solutions):
       if correct.language==2:
-        corrFile = open(DIR + 'cppfiles' + fileType + '/' + str(program_id) + 'C' + str(correct_id)+'.cpp', 'w')
+        with open(DIR + 'cppfiles' + fileType + '/' + str(program_id) + 'C' + str(correct_id) + '.cpp', 'w') as corrFile:
         corrFile.write(correct.solution)
-        corrFile.close()
-                 
-    for incorrect_id, incorrect in enumerate((problem.incorrect_solutions)):
-      if incorrect.language==2:                    
-        incorrFile = open(DIR + 'cppfiles' + fileType + '/' + str(program_id) + 'I' + str(incorrect_id)+'.cpp', 'w')
+
+    for incorrect_id, incorrect in enumerate(problem.incorrect_solutions):
+      if incorrect.language==2:
+        with open(DIR + 'cppfiles' + fileType + '/' + str(program_id) + 'I' + str(incorrect_id) + '.cpp', 'w') as incorrFile:
         incorrFile.write(incorrect.solution)
-        incorrFile.close()
+
+    with open(DIR + 'descriptions' + fileType + '/' + str(program_id) + '.txt', 'w') as descFile:
+      descFile.write(problem.description)
+    with open(DIR + 'samples' + fileType + '/' + str(program_id) + '.txt', 'w') as sampleFile:
+      sampleFile.write(str(problem.public_tests))
 
 def main():
   for fileType in FILETYPE:
@@ -40,4 +49,5 @@ def main():
     make_cpp_files(filenames, fileType)
 
 if __name__ == '__main__':
-  main()
+ main()
+
